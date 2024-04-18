6f7b1e500c917fbc82af1459b67844a12c3ccd0f
Abhinav Jain <abhinavjain2233@gmail.com>
2024-04-18 19:10:15 +0530

    Timer game Using Refs

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,62 @@
+import { useState, useRef } from 'react';
+
+import ResultModal from './ResultModal.jsx';
+
+// let timer;
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+   // since the ref is defined inside of the component function 
+    // it is component instance specific . Every Component instance Of this Timer Component 
+    // will get its timer ref which will work totally independent of each other
+  const dialog = useRef();
+
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
+  }
+
+  function handleStop() {
+    dialog.current.open();
+    clearInterval(timer.current);
+  }
+
+  return (
+    <>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerIsActive ? 'active' : undefined}>
+          {timerIsActive ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
+  );
+}
